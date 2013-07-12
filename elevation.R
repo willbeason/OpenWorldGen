8d2937b4e33250686166c19692d8eeb56aa2e215
beason4251 <informationarchitect73137@gmail.com>
2013-07-11 21:05:57 -0500

    Completed single-level noise generator
    Expanding to fractal heights should be trivial. Done for the day.
    Improvement in calculation speed over the python version is about 40%.

--- elevation.R
+++ elevation.R
@@ -7,10 +7,50 @@
 # projections.R is not required for this to run, just make testing easier
 source("projections.R", local = TRUE)
 
-test.proj <- GenProjection(c(200, 100))
+test.proj <- GenProjection(c(200, 100), radius = 5000)
+
+GetHeight <- function(pos, noise.set, roughness, depth) {
+    # Given a position and the surrounding noise, get the height.
+    # Uses linear interpolation
+    
+    cor.1 <- (-1 + floor(pos/
+                             max.feature.size)) %% noise.size + 1
+    cor.2 <- cor.1 %% noise.size + 1
+    x.n <- c(cor.1[1], cor.2[1])
+    y.n <- c(cor.1[2], cor.2[2])
+    z.n <- c(cor.1[3], cor.2[3])
+    
+    surr.noise <- noise.set[x.n, y.n, z.n]
+    rel.pos <- (pos / max.feature.size) %% 1
+    
+    x.2 <- rel.pos[1]
+    x.1 <- 1 - x.1
+    y.2 <- rel.pos[2]
+    y.1 <- 1 - y.1
+    z.2 <- rel.pos[3]
+    z.1 <- 1 - z.1
+    
+    xy.11 <- x.1 * y.1
+    xy.12 <- x.1 * y.2
+    xy.21 <- x.2 * y.1
+    xy.22 <- x.2 * y.2
+    
+    x111 <- surr.noise[1, 1, 1] * xy.11 * z.1
+    x112 <- surr.noise[1, 1, 2] * xy.11 * z.2
+    x121 <- surr.noise[1, 2, 1] * xy.12 * z.1
+    x122 <- surr.noise[1, 2, 2] * xy.12 * z.2
+    x211 <- surr.noise[2, 1, 1] * xy.21 * z.1
+    x212 <- surr.noise[2, 1, 2] * xy.21 * z.2
+    x221 <- surr.noise[2, 2, 1] * xy.22 * z.1
+    x222 <- surr.noise[2, 2, 2] * xy.22 * z.2
+    altitude <- x111 + x112 + x121 + x122 + x211 + x212 + x221 + x222
+    
+    return(altitude)
+}
 
 GenHeights <- function(pos.array, noise = "Perlin", max.feature.size = 5000,
-                       roughness = 0.25, noise.size = 10, fractal = TRUE) {
+                       roughness = 0.1, noise.size = 10, fractal = TRUE,
+                       depth = 5) {
     # pos.array is the position array created by GenProjection.
     # noise currently only accepts "Perlin".
     # max.feature.size is untested and may have unexpected effects.
@@ -21,6 +61,15 @@ GenHeights <- function(pos.array, noise = "Perlin", max.feature.size = 5000,
     # I'm currently using a scaling factor of 3 for converting from random
     # numbers to heights. May add an option later.
     
-    noise.set <- array(3*rnorm((noise.size + 1)^3), dim=rep(noise.size + 1, 3))
+    noise.set <- array(3*rnorm(noise.size^3), dim=rep(noise.size, 3))
+    the.dim <- dim(pos.array)[1:2]
+    altitude.array <- array(integer(prod(the.dim)), dim=the.dim)
     
+    for (y in 1:dim(pos.array)[1]) {
+        for (x in 1:dim(pos.array)[2]) {
+            altitude.array[y, x] <- GetHeight(pos.array[y, x, ], noise.set,
+                                              roughness, depth)
+        }
+    }
+    return(altitude.array)
 }
